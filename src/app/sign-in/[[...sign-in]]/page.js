0b3e7430a93066318b451331631bbdf48d2fb15a
AMY TUCK <amyltuck@hotmail.com>
2026-03-18 14:37:00 +0000

    style: adjust global stylesheet for sign in and sign up clerk components

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,11 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
-import signInStyles from "@/app/styles/clerksigninup.module.css";
 
 export default function SignInPage() {
   return (
     <main>
-      <section className="flex justify-center mb-4">
-        <SignIn className={signInStyles} />
+      <section className="flex justify-center mb-4 mt-4">
+        <SignIn />
       </section>
     </main>
   );
--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,12 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
-import signUpStyles from "@/app/styles/clerksigninup.module.css";
 
 export default function SignUpPage() {
   return (
     <main>
       <h1>Never been here before? Sign up to start sharing your small wins!</h1>
-      <section className="flex justify-center mb-4">
-        <SignUp className={signUpStyles} />
+      <section className="flex justify-center mb-4 mt-4">
+        <SignUp />
       </section>
     </main>
   );
